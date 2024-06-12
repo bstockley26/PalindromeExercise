43f8ff35faf6a07be5efdcd89825285fed553c14
Brooks Stockley <bstockley26@gmail.com>
2024-06-12 14:28:20 -0400

    finished

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace PalindromeExercise
+{
+	public class WordSmith
+	{
+		
+
+        public bool IsAPalindrome(string word)
+		{
+			var reversed = "";
+
+			for (int i=word.Length-1;i>=0; i--)
+			{
+				reversed += word[i];
+			}
+
+			if (word.ToLower() == reversed.ToLower())
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+}
+
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,28 @@
-using System;
+using System.Linq;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("hello", false)]
+        [InlineData("wow", true)]
+        [InlineData ("hat",false)]
+        [InlineData("radar", true)]
+        [InlineData("soccer",false)]
 
+
+        public void PalodromeTest(string word, bool expected)
+        {
+            var wordsmith = new WordSmith();
+
+            var actual = wordsmith.IsAPalindrome(word);
+
+            Assert.Equal(expected, actual);
+            
         }
     }
 }
